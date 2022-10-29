da5838fdafc62c67ed50c7c66d0b9db9cbbf7eb1
choongman.choi <choongman.choi@kweather.co.kr>
2022-10-29 17:27:03 +0900

    'refactoring11'

---
+++ refacotring/11.API리팩터링/11.13예외를 사전확인으로 바꾸기.js
@@ -0,0 +1,17 @@
+// Replace Exception with Precheck
+
+double getValueForPeriod (int periodNumber) {
+    try {
+
+    }
+    catch {
+
+    }
+}
+
+// 절차
+// 예외를 유발하는 상황을 검사할 수 있는 조건문을 추가, catch 블록의 코드를 조건문의 조건절 중 하나로 옮기고, 남은 try블록 코드를 다른 조건절로 옮김
+// catch 블록에 어서션을 추가하고 테스트
+// try문과 catch 블록을 제거
+// 테스트
+
