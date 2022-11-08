c00a626dbab3ef84bee35a49a46d98711929821a
choongman.choi <choongman.choi@kweather.co.kr>
2022-11-08 19:00:56 +0900

    'part1_complete'

--- pythonhandledata/1.seoul/seoul.py
+++ pythonhandledata/1.seoul/seoul.py
@@ -31,13 +31,52 @@ import matplotlib.pyplot as plt
 
 
 #plt.figure
-#plt.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3 ,2, 1])
+#plt.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3 ,2, 
 #plt.show()
 
 
-t = np.arange(0, 12, 0.01)
-y = np.sin(t)
+#t = np.arange(0, 12, 0.01)
+#y = np.sin(t)
 
-plt.figure
-plt.plot(t, y)
+# plt.figure
+# plt.plot(t, y)
+# plt.show()
+
+# plt.figure(figsize = (10, 6))
+# plt.plot(t, y)
+# plt.grid() # 격자무늬
+# plt.xlabel('time')
+# plt.ylabel('Amplitude')
+# plt.title('Example of sinewave') # 제목
+# plt.show()
+
+# t = [0, 1, 2, 3, 4, 5, 6]
+# y = [1, 4, 5, 8, 9, 5, 3]
+# plt.figure(figsize=(10, 6))
+# plt.plot(t, y, color = 'green', linestyle='dashed', marker ='o') # 데이터가 존재하는 곳 에 마킹
+# plt.show()
+
+# t = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+# y = np.array([9, 8, 7, 9, 8, 3, 2, 4, 3, 4])
+
+# # plt.figure(figsize = (10, 6))
+# # plt.scatter(t, y)
+# # plt.show()
+
+# colormap = t
+# plt.figure(figsize = (10, 6))
+# plt.scatter(t, y, s = 50, c = colormap, marker='>')
+# plt.colorbar()
+# plt.show()
+
+s1 = np.random.normal(loc = 0, scale=1, size= 1000)
+s2 = np.random.normal(loc = 5, scale=5, size= 1000)
+s3 = np.random.normal(loc = 10, scale=2, size= 1000)
+
+plt.figure(figsize=(10, 6))
+plt.plot(s1, label='s1')
+plt.plot(s2, label='s2')
+plt.plot(s3, label='s3')
+
+plt.legend()
 plt.show()
