641e61d81f4441e0291634b392a0e72dc5ac4aa7
choongman.choi <choongman.choi@kweather.co.kr>
2022-11-12 18:43:56 +0900

    'part2_complete'

--- pythonhandledata/part2/seoul.py
+++ pythonhandledata/part2/seoul.py
@@ -4,6 +4,8 @@ import googlemaps
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import preprocessing
+from matplotlib import font_manager, rc
+import platform
 # crime_anal_police = pd.read_csv('02. crime_in_Seoul_include_gu_name.csv', thousands=',', encoding='utf-8')
 
 # gmaps_key = 'AIzaSyCHWo8bs4NOZba8gQyZUwxtdIVACr4biPU'
@@ -108,9 +110,60 @@ from sklearn import preprocessing
 # plt.plot(x,y1, x,y2, x,y3, x,y4)
 # plt.show()
 
-sns.set_style('whitegrid')
-tlps = sns.load_dataset('tips')
+# # sns.set_style('whitegrid')
+# tips = sns.load_dataset('tips')
 
-plt.figure(figsize=(8,6))
-sns.boxplot(x='day', y='total_bill', data=tlps)
-plt.show()
+# # plt.figure(figsize=(8,6))
+# # sns.boxplot(x='day', y='total_bill', data=tlps)
+# # plt.show()
+
+# sns.set_style('darkgrid')
+# sns.lmplot(x='total_bill', y='tip', hue='smoker', data=tips, palette='Set1')
+# plt.show()
+
+# flights = sns.load_dataset('flights')
+# flights = flights.pivot("month", "year", "passengers")
+# print(flights.head(5))
+
+# plt.figure(figsize=(10, 8))
+# sns.heatmap(flights, annot=True, fmt='d') #경향 설명하기 좋음
+# plt.show()
+
+# sns.set(style='ticks')
+# iris = sns.load_dataset('iris')
+# sns.pairplot(iris, hue="species")
+# plt.show()
+
+
+## 범죄 데이터 시각화하기
+# path = "c:/Windows/Fonts/malgun.ttf"
+# if platform.system() == 'Darwin':
+#     rc('font', family='AppleGothic')
+# elif platform.system() == 'Windows':
+#     font_name = font_manager.FontProperties(fname=path).get_name()
+#     rc('font', family=font_name)
+# else:
+#     print('Unknown stste,/...')
+
+#     sns.pairplot(crime_anal_norm, vars=['강도', '살인', '폭력', kind='reg', size=3]) #상관관계 그래프
+
+# folium 지도 시각화 도구
+import folium
+
+# map_osm = folium.Map(location=[45.5236, -122.6750])
+
+# stamen = folium.Map(location=[45.5236, -122.6750], tiles='Stamen Toner', zoom_start=13)
+# stamen'
+
+state_unemployment = '02. folium_US_Unemployment_Oct2012.csv'
+state_data = pd.read_csv(state_unemployment)
+
+state_geo = '02. folium_us-states.json'
+map = folium.map(location=[40, -98], zoom_start=4)
+map.choropleth(geo_data = state_geo, data=state_data,
+    columns = ['State, Unemployment'],
+    key_on  ='feature_id',
+    file_color = 'YlGn',
+    legend_name = 'Unemployment Rate (%)'
+)
+map
