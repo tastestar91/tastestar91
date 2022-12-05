f4a129ed6c9bc87e9537a44e7861612cb6204ac8
tastestar91 <84552632+tastestar91@users.noreply.github.com>
2022-12-05 20:09:44 +0900

    gitweird

--- Grokking_Simplicity/chapter11.js
+++ Grokking_Simplicity/chapter11.js
@@ -1,125 +1,72 @@
-// 10. 일급 함수
+// 11. 일급함수
 
-// 리팩터링 : 암묵적 인자를 드러내기
-// - 함수 이름에 있는 암묵적 인자를 확인
-// - 명시적인 인자를 추가
-// - 함수 본문에 하드 코딩된 값을 새로운 인자로 바꿈
-// - 함수를 호출하는 곳을 고침
+// 함수 이름에 있는 암묵적 인자
+// => 리팩터링 : 암묵적 인자를 드러내기
+// => 리팩터링 : 함수 본문을 콜백으로 바꾸기
 
-// 리팩터링 : 함수 본문을 콜백으로 바꾸기
-// - 함수 본문에서 바꿀 부분의 앞부분과 뒷부분을 확인
-// - 리팩터링 할 코드를 함수로 빼냄
-// - 빼낸 함수의 인자로 넘길 부분을 또 다른 함수로 뺴냄
+// 리팩터링
+// - 표준화된 원칙
+// - 새로운 동작에 원칙 적용
+// - 여러 개를 변경할 때 최적화
 
-// => 함수 이름에 있는 암묵적 인자
-// - 함수 구현이 거의 똑같음
-// - 함수 이름이 구현의 차이를 만듬
+function push(array, elem) {
+  // let copy = array.slice()
+  // copy.push(elem)
+  // return copy
+  return withArrayCopy(array, (copy) => {
+    copy.push(elem)
+  })
+}
+function drop_last(array) {
+  let array_copy = array.slice()
+  array_copy.pop()
+  return array_copy
+}
 
-// (1) 리팩터링 : 암묵적 인자 들어내기
+function drop_last(array) {
+  let array_copy = array.slice()
+  array_copy.shift()
+  return array_copy
+}
 
-function setFieldByName(cart, name, field, value) {
-  if (!validItemFields.includes(field)) {
-    throw "Not a valid item field";
-  }
-  if (translations.hasOwnProperty(field)) {
-    field = translations[field];
-  }
-  // function setPriceByName(cart, name, price) {
-  let item = cart[name];
-  // let newItem = objectSet(item, 'price', 13)
-  let newItem = objectSet(item, field, value);
-  let newCart = objectSet(cart, name, newItem);
-  // let newCart = objectSet(cart, name, newItme)
-  return newCart;
+
+
+function withObjectCopy(object, modify) {
+  let copy = Object.assign({}, object)
+  copy[key] = value
+  return copy
+}
+
+function withObjectCopy(object, modify) {
+  let copy = Object.assign({}, object)
+  modify(copy)
+  return copy
 }
 
 function objectSet(object, key, value) {
-  let copy = Object.assign({}, object);
-  copy[key] = value;
-  return copy;
-}
-// A. 일급인 것과 일급이 아닌 것을 구별하기
-
-// 일급이 아닌 것 : 수식 연산자, 반복문, 조건문, try/catch 블록
-// 일급인 것 : 변수에 할당, 함수의 인자로 넘기기, 함수의 리턴값으로 받기, 배열이나 객체에 담기
-
-// 필드명이 일급이라는 말은 객체나 배열에 담을 수 있다는 뜻
-
-// B. 객체와 배열을 너무 많이 쓰게 됨
-
-// 데이터 지향 : 이벤트와 엔티티에 대한 사실을 표현하기 위해 일반 데이터 구조를 사용하는 프로그래밍 형식
-// 제한된 api로 정의하면 데이터를 제대로 활용할 수 없음,
-
-// (2) 리팩터링 : 콜백으로 바꾸기
-
-// 고차함수 : 인자를 함수로 받거나 리턴 값으로 함수를 리턴할 수 있는 함수를 말함
-
-function cookAndEatFoods() {
-  for (let i = 0; i < foods.length; i++) {
-    let food = foods[i];
-    cook(food);
-    eat(food);
-  }
+  return withObjectCopy(object, (copy) => {
+    copy[key] = value
+  })
 }
 
-function cleanDishes() {
-  for (let i = 0; i < dishes.length; i++) {
-    let dish = dishes[i];
-    wasy(dish);
-    dry(dish);
-    putAway(dish);
-  }
-}
 
-function operateOnArray(array, f) {
-  for (let i = 0; i < array.length; i++) {
-    let item = array[i];
-    f(item);
-  }
-}
-
-function clean(dish) {
-  wash(dish);
-  dry(dish);
-  putAway(dish);
-}
-
-function forEach(array, f) {
-  for (let i = 0; i < array.length; i++) {
-    let item = array[i];
-    f(item);
-  }
-}
-
-// forEach는  배열 전체를 순회할 수 있는 완전한 반복문, 반복문을 다시 만들 필요없음
-// 고차함수 함수를 인자로 받음
-
-// (3) 리팩터링 : 함수 본문을 콜백으로 바꾸기
-
-// - 본문과 본문의 앞부분과 뒷부분을 구현합니다
-// - 전체를 함수로 빼냅니다
-// - 본문 부분을 빼낸 함수의 인자로 전달한 함수로 바꿈
-
-// 콜백 = 핸들러 함수
-
-function withLogging() {
+function tryCatch(f, errorHandler) {
   try {
-    f();
-  } catch (error) {
-    logToSnapErros(error);
+    return f()
+  }
+  catch(error) {
+    return errorHandler(error)
   }
 }
 
-withLogging(() => {
-  saveUserData(user);
-});
-// 인라인 함수 : 쓰는 곳에서 바로 정의하는 함수
 
-// - 전역으로 정의
-function global() {}
+// (1) 함수를 리턴하는 함수
+// - 어떤 부분에 로그를 남기는 것을 깜빡할 수 있음
+// - 모든 코드에 수동으로 withLogging() 함수를 적용해야함
 
-// - 지역으로 정의
-const local = () => {};
-// - 인라인으로 정의
-// = 익명 함수라고도 함
-inline(() => {});
+// 함수 본문을 콜백으로 바꾸기
+// 함수를 리턴하는 함수는 팩토리 함수와 같음
+
+// - 고차 함수로 패턴이나 원칙을 코드로 만들 수 있음
+// - 고차 함수로 리턴하는 함수를 만들 수 있음
+// - 고차 함수르르 사용하면서 잃는 것도 있음, 많은 중복 코드를 없애주지만 기독성을 해칠 수도 있음
--- Grokking_Simplicity/chapter13.js
+++ Grokking_Simplicity/chapter13.js
@@ -1,125 +1,108 @@
-// 10. 일급 함수
+// 13. 함수형 도구 체이닝
 
-// 리팩터링 : 암묵적 인자를 드러내기
-// - 함수 이름에 있는 암묵적 인자를 확인
-// - 명시적인 인자를 추가
-// - 함수 본문에 하드 코딩된 값을 새로운 인자로 바꿈
-// - 함수를 호출하는 곳을 고침
+// function biggestPurchaseBestCustomers (customers) {
+//   let bestCustomers = filter(customers, function(customer) {
+//     return customer.purchases.length >= 3
+//   })
+//   let biggestPurchases = map(bestCustomers, (customer) => {
+//     return reduce(customer.reduce, {total : 0}, (biggestsSoFar, purchases) => {
+//       if (biggestsSoFar.total > purchases.total) {
+//         return biggestsSoFar
+//       }
+//       else {
+//         return purchases
+//       }
+//     })
+//   })
+//   return biggestPurchases
+// }
 
-// 리팩터링 : 함수 본문을 콜백으로 바꾸기
-// - 함수 본문에서 바꿀 부분의 앞부분과 뒷부분을 확인
-// - 리팩터링 할 코드를 함수로 빼냄
-// - 빼낸 함수의 인자로 넘길 부분을 또 다른 함수로 뺴냄
-
-// => 함수 이름에 있는 암묵적 인자
-// - 함수 구현이 거의 똑같음
-// - 함수 이름이 구현의 차이를 만듬
-
-// (1) 리팩터링 : 암묵적 인자 들어내기
-
-function setFieldByName(cart, name, field, value) {
-  if (!validItemFields.includes(field)) {
-    throw "Not a valid item field";
+reduce(customer.purchases, { total : 0}, (biggestsSoFar, purchases) => {
+  if (biggestsSoFar.total > purchases.total) {
+    return biggestsSoFar
   }
-  if (translations.hasOwnProperty(field)) {
-    field = translations[field];
+  else {
+    return biggestsSoFar
   }
-  // function setPriceByName(cart, name, price) {
-  let item = cart[name];
-  // let newItem = objectSet(item, 'price', 13)
-  let newItem = objectSet(item, field, value);
-  let newCart = objectSet(cart, name, newItem);
-  // let newCart = objectSet(cart, name, newItme)
-  return newCart;
+})
+function mapKey(array, init, f) {
+  return reduce(array, init, (biggestsSoFar, element) => {
+    if (biggestsSoFar.total > purchases.total) {
+      return biggestsSoFar
+    }
+    else {
+      return biggestsSoFar
+    } 
+  })
 }
 
-function objectSet(object, key, value) {
-  let copy = Object.assign({}, object);
-  copy[key] = value;
-  return copy;
-}
-// A. 일급인 것과 일급이 아닌 것을 구별하기
-
-// 일급이 아닌 것 : 수식 연산자, 반복문, 조건문, try/catch 블록
-// 일급인 것 : 변수에 할당, 함수의 인자로 넘기기, 함수의 리턴값으로 받기, 배열이나 객체에 담기
-
-// 필드명이 일급이라는 말은 객체나 배열에 담을 수 있다는 뜻
-
-// B. 객체와 배열을 너무 많이 쓰게 됨
-
-// 데이터 지향 : 이벤트와 엔티티에 대한 사실을 표현하기 위해 일반 데이터 구조를 사용하는 프로그래밍 형식
-// 제한된 api로 정의하면 데이터를 제대로 활용할 수 없음,
-
-// (2) 리팩터링 : 콜백으로 바꾸기
-
-// 고차함수 : 인자를 함수로 받거나 리턴 값으로 함수를 리턴할 수 있는 함수를 말함
-
-function cookAndEatFoods() {
-  for (let i = 0; i < foods.length; i++) {
-    let food = foods[i];
-    cook(food);
-    eat(food);
-  }
+function max(array, init) {
+  return mapKey(array, init, (x) => {
+    return x
+  })
 }
 
-function cleanDishes() {
-  for (let i = 0; i < dishes.length; i++) {
-    let dish = dishes[i];
-    wasy(dish);
-    dry(dish);
-    putAway(dish);
-  }
+// 인자로 받은 값을 그대로 리턴하는 함수를 '항등 함수' 라고 함 (identity function)
+
+// (1) 체인을 명확하게 만들기 1 : 단계에 이름 붙이기
+
+function biggestPurchaseBestCustomers (customers) {
+  // let bestCustomers = selectBestCustomers(customers) // 1 단계
+  let bestCustomers = filter(customers, isGoodCustomer)
+  let biggestPurchases = getBiggestPurchases(bestCustomers) // 2 단계
+  return biggestPurchases
 }
 
-function operateOnArray(array, f) {
-  for (let i = 0; i < array.length; i++) {
-    let item = array[i];
-    f(item);
-  }
+function isGoodCustomer(customer) {
+  return customer.purchases.length >= 3
 }
 
-function clean(dish) {
-  wash(dish);
-  dry(dish);
-  putAway(dish);
+function selectBestCustomers(customers) {
+  return filter(customers, (customer) => {
+    return customer.purchases.length >= 3
+  })
 }
 
-function forEach(array, f) {
-  for (let i = 0; i < array.length; i++) {
-    let item = array[i];
-    f(item);
-  }
+function getBiggestPurchases(customers) {
+  return map(customers, getBiggestPurchases)
 }
 
-// forEach는  배열 전체를 순회할 수 있는 완전한 반복문, 반복문을 다시 만들 필요없음
-// 고차함수 함수를 인자로 받음
-
-// (3) 리팩터링 : 함수 본문을 콜백으로 바꾸기
-
-// - 본문과 본문의 앞부분과 뒷부분을 구현합니다
-// - 전체를 함수로 빼냅니다
-// - 본문 부분을 빼낸 함수의 인자로 전달한 함수로 바꿈
-
-// 콜백 = 핸들러 함수
-
-function withLogging() {
-  try {
-    f();
-  } catch (error) {
-    logToSnapErros(error);
-  }
+function getBiggestPurchase(customer) {
+  // return mapKey(customer.purchases, { total : 0 }, (purchases) => {
+  //   return purchases.total
+  // })
+  return maxKey(customer.purchases, { total : 0}, getPurchaseTotal)
 }
 
-withLogging(() => {
-  saveUserData(user);
-});
-// 인라인 함수 : 쓰는 곳에서 바로 정의하는 함수
+function getPurchaseTotal(purchases) {
+  return purchases.total
+}
+// (2) 체인을 명확하게 만들기 : 콜백에 이름 붙이기
 
-// - 전역으로 정의
-function global() {}
+// (3) 체인을 명확하게 만들기 3: 두 방법을 비교
+// 방법 1 : 단계에 이름 붙이기
 
-// - 지역으로 정의
-const local = () => {};
-// - 인라인으로 정의
-// = 익명 함수라고도 함
-inline(() => {});
+// 방법 2 : 콜백에 이름 붙이기
+// 두반째 방법이 더 명확함, 고차 함수를 그대로 쓰는 첫 번째 방법보다 이름을 붙인 두번째 방법이 재사용하기도 좋음
+
+let firstTimers = filter(customers, (customer) => {
+  return customer.purchases.length === 1
+  // filter(customers, isFirstTimer)
+})
+let firstTimerEmails = map(firstTimers, (customer) => {
+  return customer.email
+  // map(firstTimers, getCustomerEmail)
+})
+
+function isFirrstTimer(customer) {
+  return customer.purchases.length === 1
+}
+
+function getCustomerEmail(customer) {
+  return customer.email
+}
+
+// filter와 map은 모두 새로운 배열을 만듬, 
+// map filter reduce체인을 최적화하는 걸 '스트림 결합'이라고 함
+
+// 두개 중복할 경우, 오른쪽 코드가 가비지 컬렉션을 적게함
